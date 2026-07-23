83a9217695b13494f4a008ddda441924c8241946
Jhonnathan Ocampo <jsocampod@eafit.edu.co>
2026-07-23 17:34:33 -0500

    feat: set up compiled views path and environment variables for view rendering

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
+$compiledViewsPath = '/tmp/views';
+
+if (! is_dir($compiledViewsPath)) {
+    mkdir($compiledViewsPath, 0755, true);
+}
+
+putenv('VIEW_COMPILED_PATH='.$compiledViewsPath);
+$_ENV['VIEW_COMPILED_PATH'] = $compiledViewsPath;
+$_SERVER['VIEW_COMPILED_PATH'] = $compiledViewsPath;
+
 require __DIR__.'/../public/index.php';
